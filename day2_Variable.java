f653fc6ed88bf6c3bef4361d2a07a7345b013db2
Soban Abbas <sobanabbass@gmail.com>
2025-09-18 00:05:46 +0500

    learning veriables

---
+++ day2_Variable.java
@@ -0,0 +1,15 @@
+public class day2_Variable {
+    public static void main(String[] args) {
+        int regNo=4705;  //int store integers
+        String name ="Soban";  //text
+        boolean degreeCompleted=false;
+        double gpa= 3.4;
+
+        System.out.println("ReG NO: " + regNo);
+        System.out.println("Name: " +name);
+        System.out.println("CGPA: "+ gpa);
+        System.out.println("Degree completed:? " + degreeCompleted);
+
+
+    }
+}

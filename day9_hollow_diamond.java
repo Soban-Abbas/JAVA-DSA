a1870d1198c9d88f9bd5d7bb7fb7a9a950445956
Soban Abbas <sobanabbass@gmail.com>
2025-10-04 11:58:07 +0500

    hollow diamond

---
+++ day9_hollow_diamond.java
@@ -0,0 +1,39 @@
+public class day9_hollow_diamond {
+    public static void main(String[] args) {
+        int rows=5;
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            if(i==1){
+                System.out.print("*");
+                System.out.println();
+            }else{
+                System.out.print("*");
+                for(int j=1;j<=2*i-1-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                System.out.println();
+
+            }
+           
+        }
+        for(int i=rows-1;i>=1;i--){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            if(i==1){
+                System.out.print("*");
+                System.out.println();
+            }else{
+                System.out.print("*");
+                for(int j=1;j<=2*i-1-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                System.out.println();
+            }
+        }
+    }
+}
